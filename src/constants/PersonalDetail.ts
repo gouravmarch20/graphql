3de4c8c0025969344f4b9fe5f8162db55e7a2a65
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2025-12-09 14:18:55 +0530

    link change

--- src/constants/PersonalDetail.ts
+++ src/constants/PersonalDetail.ts
@@ -1,7 +1,7 @@
 export const PersonalDetails
  = {
     github:"https://github.com/gouravmarch20",
-    linkedin:"https://linkedin.com/gouravmarch20",
+    linkedin:"https://www.linkedin.com/in/gouravmarch20/",
     email:"gouravmarch20@gmail.com",
     name:"Gourav Mishra",
     profilePic:"/gouravmarch20.jpg",
